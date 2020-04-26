55419c1cf9f4803f5862fcd6f5cb660d4b86bf86
Alexander <alex.lisitsa7@gmail.com>
2020-04-26 17:24:36 +0300

    Map with scooters(dynamic)

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,23 +1,91 @@
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-import React from "react";
-import {Map, TileLayer} from 'react-leaflet';
-
-
+import Button from '@material-ui/core/Button';
+import React, {useState, useEffect} from "react";
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
+import axios from "axios";
+import {API_BASE_URL} from "../constants/apiConstants";
 
 export default function MapComponent() {
 
   const size = { width: "100vw", height: "100vh" };
+  const [scooters, setScooters] = useState([]);
+  const [chosenScooter, setChosen] = useState({});
+  const [pointNewScooter, setNewScooterPoint] = useState({
+    stLat: '',
+    stLon: '',
+    battery: 0
+  })
+  const [scenarioDestination, setDestination] = useState({
+    destLat: '',
+    destLon: '',
+    dischInd: 3,
+    scooterId: ''
+  })
+  const [simulationMode, setSimMode] = useState();
+  const [createScooterMode, setCreateScooterMode] = useState(false);
 
-    return (
-        // <Container>
-            // <Grid item xs='12'>
-              <Map center={{ lat: 48.464970, lng: 35.046537 }} zoom={13} style={size}>
-                <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-              </Map>
-            //  </Grid>
-        //  </Container>
-    );
+
+  useEffect(() => {
+
+  const interval = setInterval(() => {
+      axios.get(API_BASE_URL + 'vehicle-service/scooters/status/free')
+    .then(response => {
+        setScooters(response.data)
+        console.log(response.data)
+    })
+    .catch((error) =>
+        console.log(error)
+    )
+  }, 1000);
+
+   return () => clearInterval(interval);
+    
+   });  
+ 
+ 
+   const handleChooseScooterClick = (id) => {
+      setChosen(id);
+   };
+ 
+ 
+   const addTestScooter = (e) => {
+     if(createScooterMode){
+       setNewScooterPoint()
+       }
+     console.log(coordinates)
+ 
+   }  
+
+   const coordinates = [48.464970, 35.046537]
+ 
+   return (
+     <>
+       <Map 
+         center={coordinates} 
+         onClick={addTestScooter}
+         zoom={13} 
+         style ={size}
+         >
+         <TileLayer
+           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+         />
+
+         {scooters.map((p) => (
+          <Marker position={p.coordinates}>
+          <Popup>
+            <span>A pretty CSS3 popup. <br/> Easily customizable.       
+            <br/>
+            Battery: {p.battery}<br/>
+            Id: {p.id}
+            <Button onClick={handleChooseScooterClick}>Choose</Button>
+            </span> 
+          </Popup>
+        </Marker>
+         ))}
+ 
+       </Map>
+       </>
+     );
 }
